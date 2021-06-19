3f3aec3391755ac8d24fb19850e126f1bc38bb69
jake <jakehallam95@gmail.com>
2021-06-19 18:05:59 +0100

    Added in bashrc config

--- configs/setup_configs.sh
+++ configs/setup_configs.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 # Add a symlink, to this .vimrc file, in the $VIM folder
-rm ~/.vimrc -f
-ln -s "$(pwd)/.vimrc" ~/.vimrc
+rcFiles=(
+    .vimrc
+    .bashrc
+)
+
+for rc in ${rcFiles[@]}; do
+    rm ~/$rc -f
+    ln -s "$(pwd)/$rc" ~/$rc
+done;
+
 git config --global credential.helper cache
