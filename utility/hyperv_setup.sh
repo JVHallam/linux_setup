72889e7ed2e7df5cbdba3d69a8dad546f79b3f0c
ubuntu <email>
2022-01-01 14:37:21 +0000

    Add utility scripts

--- utility/hyperv_setup.sh
+++ utility/hyperv_setup.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 
-confFilePath="/etc/modprobe.d/blacklist.conf"
-hypervBlacklistString="blacklist hyperv_fb"
-
 sudo apt update && sudo apt upgrade
 
-# Fix the display
-## Make it not laggy
-sudo grep -q "$hypervBlacklistString" $confFilePath
+# Adding in the 1920x1080 process
+grubConfPath="/etc/default/grub"
 
-#If we don't find it
-if [ $? -eq 1 ]
-then
-    sudo echo "blacklist hyperv_fb" | sudo tee -a /etc/modprobe.d/blacklist.conf
-else
-    echo "Hyper V already blacklisted"
-fi
+# hyperv-drivers?
+sudo apt-get install linux-image-extra-virtual
 
 # Fix the install
 # I don't know if this is needed
---
+++ utility/update_grub.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo vim /etc/default/grub
+sudo update-grub
